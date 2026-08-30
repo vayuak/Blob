6709e3ebfa8e1d779644e2acecd9d0291d150d81
vayuak <adityakakade7350@gmail.com>
2026-08-30 23:12:24 +0530

    dfghfrr5tyfgcdeertygh

--- src/main/java/com/media_vault_service/Blob/BlobApplication.java
+++ src/main/java/com/media_vault_service/Blob/BlobApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
-
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class BlobApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlobApplication.class, args);
